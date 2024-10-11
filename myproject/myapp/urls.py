68f08acfaea54e102cf6ef8be045e8526cadb3c1
unknown <bexruzraxmanov59@gmail.com>
2024-10-11 15:31:08 +0500

    list

--- myproject/myapp/urls.py
+++ myproject/myapp/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from .views import create
+from .views import create,list,update
 
 urlpatterns = [
-    path('',create,name='create'),
+    path('create/',create,name='create'),
+    path('',list,name='list'),
+    path('update/<int:pr_id>/',update,name='update')
 ]
--- myproject/myapp/views.py
+++ myproject/myapp/views.py
@@ -13,5 +13,19 @@ def create(request):
         form = ProductForm()
     return render(request,'form.html',{'form':form})
 
+def list(request):
+    product = Product.objects.all()
+    return render(request,'list.html',{'product':product})
+
+def update(request,pr_id):
+    product = Product.objects.get(id=pr_id)
+    if request.method == 'POST':
+        form = ProductForm(request.POST,instance=product)
+        if form.is_valid():
+            form.save()
+            return redirect('list')
+    else:
+        form = ProductForm(instance=product)
+    return render(request,'form.html',{'form':form})
 
         
